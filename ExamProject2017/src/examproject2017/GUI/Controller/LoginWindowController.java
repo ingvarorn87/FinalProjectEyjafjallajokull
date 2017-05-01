a38b9f4d767bf3887bdcc98d38a6fd5acbf3e108
Gudlauggu <gudl0025@easv365.dk>
2017-05-01 11:07:01 +0200

    more fun with css and conecting windows

---
+++ ExamProject2017/src/examproject2017/GUI/Controller/LoginWindowController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examproject2017.GUI.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * FXML Controller class
+ *
+ * @author gudla
+ */
+public class LoginWindowController implements Initializable
+{
+
+    @FXML
+    private Button btnAdminLogin;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private PasswordField txtPass;
+    @FXML
+    private Button btnLogin;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+      {
+        txtPass.setVisible(false);
+        // TODO
+      }    
+
+    @FXML
+    private void adminLogin(ActionEvent event)
+      {
+        txtPass.setVisible(true);
+      }
+
+    @FXML
+    private void login(ActionEvent event) throws IOException
+      {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/examproject2017/GUI/View/SelectedVolunteerWindow.fxml"));
+                Parent root = loader.load();
+                SelectedVolunteerWindowController vController = (SelectedVolunteerWindowController) loader.getController();
+                
+
+                Stage subStage = new Stage();
+                subStage.setScene(new Scene(root));
+
+
+                subStage.show();
+                Stage stage = (Stage) btnLogin.getScene().getWindow();
+                stage.close();
+        
+      }
+    
+}
