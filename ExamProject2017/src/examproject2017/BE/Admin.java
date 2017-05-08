2c8b8efd4de4bd33427d82988421a394d423a62a
ingvarorn87 <ingvarorn87@gmail.com>
2017-05-08 11:03:02 +0200

    Admin class added

---
+++ ExamProject2017/src/examproject2017/BE/Admin.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examproject2017.BE;
+
+/**
+ *
+ * @author Ingvar
+ */
+public class Admin extends Person {
+    
+    private String password;
+    
+    public Admin(String name, String email, int id, String phone, String address) {
+        
+        super(name, email, id, phone, address);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
